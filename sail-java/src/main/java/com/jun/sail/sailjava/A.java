e328e144e0105de8b451ec41e9c10590b2afa3e6
spiritn <40299015+spiritn@users.noreply.github.com>
2020-04-12 12:08:03 +0800

    测试2

--- sail-java/src/main/java/com/jun/sail/sailjava/A.java
+++ sail-java/src/main/java/com/jun/sail/sailjava/A.java
@@ -8,6 +8,8 @@ public class A {
 
     public static void main(String[] args) {
         System.out.println("niao a ");
+        int a = 3;
+        System.out.println(Thread.currentThread().getName());
     }
 
 
